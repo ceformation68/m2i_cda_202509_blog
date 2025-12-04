59ed4f51b3840e5918fd0c65c24a261eca86aa78
Christel Ehrhart <contact@ce-formation.com>
2025-12-04 16:26:47 +0100

    connexion à la base de données en singleton

--- models/mother_model.php
+++ models/mother_model.php
@@ -1,22 +1,30 @@
 <?php
+
+    /**
+     * Classe mère des modèles
+     * Basé sur un Singleton puisqu'on teste l'instance
+     */
 	class Model_mother {
 		protected object $_db;
-	
-		public function __construct(){
-			try{
-				// Connexion à la base de données
-				$this->_db = new PDO(
-					"mysql:host=localhost;dbname=blog_php", // Serveur et BDD
-					"root", //Nom d'utilisateur de la base de données
-					"",// Mot de passe de la base de données
-					array(PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC) // Mode de renvoi
-				);
-				// Pour résoudre les problèmes d’encodage
-				$this->_db->exec("SET CHARACTER SET utf8");
-				// Configuration des exceptions
-				$this->_db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			} catch(PDOException $e) {
-				echo"Échec : " . $e->getMessage();
-			}
-		}
-	}
+        private static ?PDO $_dbInstance = null;
+
+        public function __construct(){
+            // Récupère ou crée la connexion singleton
+            if (self::$_dbInstance === null) {
+                try{
+                    self::$_dbInstance = new PDO(
+                        "mysql:host=localhost;dbname=blog_php",
+                        "root",
+                        "",
+                        array(PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC)
+                    );
+                    self::$_dbInstance->exec("SET CHARACTER SET utf8");
+                    self::$_dbInstance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                } catch(PDOException $e) {
+                    echo "Échec : " . $e->getMessage();
+                }
+            }
+            // Assigne la connexion singleton
+            $this->_db = self::$_dbInstance;
+        }
+    }
